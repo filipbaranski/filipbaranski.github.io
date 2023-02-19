40b9b99ecfd003623813f42d7f3bc8877580fabc
Filip <f.g.baranski@gmail.com>
2023-02-19 19:03:26 +0100

    PWA attempt #1

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -3,7 +3,7 @@
 import { register } from 'register-service-worker';
 
 if (import.meta.env.PROD === true) {
-  register(`${import.meta.env.BASE_URL}service-worker.js`, {
+  register(`${import.meta.env.BASE_URL}/service-worker.js`, {
     ready() {
       console.log(
         'App is being served from cache by a service worker.\n'
