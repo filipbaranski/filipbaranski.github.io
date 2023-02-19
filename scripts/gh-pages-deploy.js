8ae6e56a6eca306f0e9e408ff43ef71ddbc298ab
Filip <f.g.baranski@gmail.com>
2023-02-19 18:14:21 +0100

    Change 4

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -13,10 +13,10 @@ import fs from "fs";
     await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
     console.log("Pushing to gh-pages...");
     await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
-    await execa("rm", ["-r", folderName]);
     await execa("git", ["checkout", "-f", "master"]);
     await execa("git", ["branch", "-D", "gh-pages"]);
     console.log("Successfully deployed, check your settings");
+    console.log("Remember to remove dist folder");
   } catch (e) {
     // eslint-disable-next-line no-console
     console.log(e.message);
