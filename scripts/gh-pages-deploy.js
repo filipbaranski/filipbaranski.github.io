8ca7e4f3872cab51fcfa7da8e625cad948e3d493
Filip <f.g.baranski@gmail.com>
2023-02-24 18:22:08 +0100

    Modified scripts

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -1,25 +1,25 @@
-/* eslint-disable no-console */
 import { execa } from "execa";
 import fs from "fs";
 (async () => {
   try {
     await execa("git", ["checkout", "--orphan", "gh-pages"]);
-    // eslint-disable-next-line no-console
     console.log("Building started...");
     await execa("npm", ["run", "build"]);
-    // Understand if it's dist or build folder
     const folderName = fs.existsSync("dist") ? "dist" : "build";
+    console.log("Adding to work tree...");
     await execa("git", ["--work-tree", folderName, "add", "--all"]);
+    console.log("Committing code...");
     await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
     console.log("Pushing to gh-pages...");
     await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
     await execa("git", ["checkout", "-f", "master"]);
+    console.log("Removing local gh-pages...");
     await execa("git", ["branch", "-D", "gh-pages"]);
-    console.log("Successfully deployed, check your settings");
+    console.log("Successfully deployed");
+    console.log("Removing local /dist folder...");
     fs.rmSync('./dist', { recursive: true, force: true });
     console.log("Successfully removed temporary files");
   } catch (e) {
-    // eslint-disable-next-line no-console
     console.log(e.message);
   }
 })();
