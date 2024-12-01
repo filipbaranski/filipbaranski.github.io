e978e9e03ccea5a58b8af79b4d831266496f9c7e
filipbaranski <f.g.baranski@gmail.com>
2024-12-01 10:59:54 +0100

    gh-pages

---
+++ assets/MainLayout-Ce8SxjSO.js
@@ -0,0 +1,16 @@
+import{d as i,o as p,c as l,p as t,w as o,E as c,h as s,R as d,B as f,a as h,e as a,n as C,F as v,r as M}from"./index-oIQGwWOo.js";import{_}from"./_plugin-vue_export-helper-DlAUqK2U.js";const x={class:"menu-desktop"},w=i({__name:"DesktopMenuComponent",setup(u){return(e,n)=>(p(),l("nav",x,[t(s(d),{to:"/calendar"},{default:o(()=>n[0]||(n[0]=[c("Kalendarz")])),_:1}),t(s(d),{to:"/events"},{default:o(()=>n[1]||(n[1]=[c("Daty")])),_:1})]))}}),b=_(w,[["__scopeId","data-v-af7da06e"]]),k={},z={version:"1.1",id:"Layer_1",xmlns:"http://www.w3.org/2000/svg","xmlns:xlink":"http://www.w3.org/1999/xlink",x:"0px",y:"0px",viewBox:"0 0 297 297",style:{"enable-background":"new 0 0 297 297"},"xml:space":"preserve"};function g(u,e){return p(),l("svg",z,e[0]||(e[0]=[f(`<g data-v-ad4c04dd><path d="M279.368,24.726H102.992c-9.722,0-17.632,7.91-17.632,\r
+            17.632V67.92c0,9.722,7.91,17.632,17.632,17.632h176.376c9.722,\r
+            0,17.632-7.91,17.632-17.632V42.358C297,32.636,289.09,24.726,279.368,\r
+            24.726z" data-v-ad4c04dd></path><path d="M279.368,118.087H102.992c-9.722,0-17.632,7.91-17.632,\r
+            17.632v25.562c0,9.722,7.91,17.632,17.632,17.632h176.376c9.722,\r
+            0,17.632-7.91,17.632-17.632v-25.562C297,125.997,289.09,118.087,\r
+            279.368,118.087z" data-v-ad4c04dd></path><path d="M279.368,211.448H102.992c-9.722,0-17.632,7.91-17.632,\r
+            17.633v25.561c0,9.722,7.91,17.632,17.632,17.632h176.376c9.722,\r
+            0,17.632-7.91,17.632-17.632v-25.561C297,219.358,289.09,211.448,\r
+            279.368,211.448z" data-v-ad4c04dd></path><path d="M45.965,24.726H17.632C7.91,24.726,0,32.636,0,42.358V67.92c0,\r
+            9.722,7.91,17.632,17.632,17.632h28.333c9.722,0,17.632-7.91,\r
+            17.632-17.632V42.358C63.597,32.636,55.687,24.726,45.965,24.726z" data-v-ad4c04dd></path><path d="M45.965,118.087H17.632C7.91,118.087,0,125.997,0,135.719v25.562c0,\r
+            9.722,7.91,17.632,17.632,17.632h28.333c9.722,0,17.632-7.91,\r
+            17.632-17.632v-25.562C63.597,125.997,55.687,118.087,45.965,118.087z" data-v-ad4c04dd></path><path d="M45.965,211.448H17.632C7.91,211.448,0,219.358,0,229.081v25.561c0,\r
+            9.722,7.91,17.632,17.632,17.632h28.333c9.722,0,17.632-7.91,\r
+            17.632-17.632v-25.561C63.597,219.358,55.687,211.448,45.965,211.448z" data-v-ad4c04dd></path></g>`,1)]))}const V=_(k,[["render",g],["__scopeId","data-v-ad4c04dd"]]),y=i({__name:"MobileMenuComponent",setup(u){const e=h({open:!1}),n=()=>{e.open=!0},m=()=>{e.open=!1};return(D,r)=>(p(),l(v,null,[a("button",{class:"menu-bar",onClick:n},[t(V)]),a("nav",{class:C(["menu-mobile",{open:e.open}]),onClick:m},[t(s(d),{to:"/calendar"},{default:o(()=>r[0]||(r[0]=[c("Kalendarz")])),_:1}),t(s(d),{to:"/events"},{default:o(()=>r[1]||(r[1]=[c("Daty")])),_:1})],2)],64))}}),H=_(y,[["__scopeId","data-v-38b33196"]]),B={class:"header"},$={class:"innerBorder"},I=i({__name:"MainLayout",setup(u){return(e,n)=>(p(),l(v,null,[a("header",B,[t(s(d),{to:"/"},{default:o(()=>n[0]||(n[0]=[a("p",{class:"banner"},"Tea Hub",-1)])),_:1})]),t(b),t(H),a("section",$,[M(e.$slots,"default",{},void 0)])],64))}}),E=_(I,[["__scopeId","data-v-9fd95cbc"]]);export{E as M};
